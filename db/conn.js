7f5055b06d2fcdafb85d2a4074de060f10a50640
skero2004 <toyatakahashi522@gmail.com>
2023-01-03 22:56:34 +0900

    updated frontend fetch url

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-const Db = "mongodb+srv://cfgthrive:L3zRKcTDLkWlKK6i@cluster0.uhkxs7c.mongodb.net/?retryWrites=true&w=majority";
+const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
   	useNewUrlParser: true,
   	useUnifiedTopology: true,
