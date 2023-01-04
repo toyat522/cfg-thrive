128fd1fc5292a635a96b867917ec45bb8e582a7c
skero2004 <toyatakahashi522@gmail.com>
2023-01-04 22:33:40 +0900

    added chartjs and sample code

--- client/src/Stats.js
+++ client/src/Stats.js
@@ -2,7 +2,67 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 import Chart from 'chart.js/auto'
-import { Line } from 'react-chartjs-2'
+import { Line, Bar, Pie } from 'react-chartjs-2'
+
+const LineChart = () => {
+	const labels = ["January", "February", "March", "April", "May", "June"];
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				label: "My First dataset",
+				backgroundColor: "rgb(255, 99, 132)",
+				borderColor: "rgb(255, 99, 132)",
+				data: [0, 10, 5, 2, 20, 30, 45],
+			},
+		],
+	};
+	return (
+		<div style={{justifyContent: 'center', display: 'flex'}}>
+			<Line data={data} />
+		</div>
+	);
+};
+
+const BarChart = () => {
+	const labels = ["January", "February", "March", "April", "May", "June"];
+	const data = {
+		labels: labels,
+		datasets: [
+		{
+			label: "My First dataset",
+			backgroundColor: "rgb(255, 99, 132)",
+			borderColor: "rgb(255, 99, 132)",
+			data: [0, 10, 5, 2, 20, 30, 45],
+		},
+		],
+	};
+	return (
+		<div>
+		<Bar data={data} />
+		</div>
+	);
+};
+
+const PieChart = () => {
+	const labels = ["January", "February", "March", "April", "May", "June"];
+	const data = {
+	labels: labels,
+	datasets: [
+		{
+		label: "My First dataset",
+		backgroundColor: "rgb(255, 99, 132)",
+		borderColor: "rgb(0,0,255)",
+		data: [0, 10, 5, 2, 20, 30, 45],
+		},
+	],
+	};
+	return (
+		<div>
+		<Pie data={data} />
+		</div>
+	);
+};
 
 const Stats = () => {
 
@@ -26,6 +86,9 @@ const Stats = () => {
 				style={{margin: '1rem 1rem 1rem'}}>
 				Back to home
 			</button>
+          	<LineChart />
+          	<BarChart />
+          	<PieChart />
 		</>
 	)
 }
